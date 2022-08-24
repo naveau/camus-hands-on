70534b82cb4c3b5e5495ea24cde2b95a791b9e1d
Olivier Bernard <olivier.bernard@insa-lyon.fr>
2022-08-24 17:16:20 +0200

    update second notebook

--- keras_unet/utils.py
+++ keras_unet/utils.py
@@ -4,10 +4,103 @@ import numpy as np
 import glob
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras_unet.losses import dice_loss, dice_coef, adaptive_loss
+from tensorflow.keras import models
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+
+# Select the pre-trained U-Net model
+def select_model(id, num_classes, img_size):
+    
+    # Set the size of the input image
+    input_shape = (img_size, img_size, 1)            
+    
+    # Default network architecture values 
+    NBFILTERS_L1_UNET = 32   # Number of feature maps for the first level
+    NBLAYERS_UNET = 4        # Number of levels
+    DROPOUT_RATE = 0.1       # Use of dropout action at each level (value between 0.0 and 1.0, 0.0 meaning no dropout)
+    BATCHNORM_ON = True      # Use of batch normalisation after each convolutional layer (value = True or False)
+    BATCH_SIZE = 32          # Number of sample in each batch (usually the value is a multiple of 2)    
+    DATA_AUGMENTATION = True # Use of data augmentation during the training process
+    ACTIVATION = 'Relu'
+    RMS_OPT = False
+    LR = 0.001
+    
+    if id==2:
+        NBFILTERS_L1_UNET = 16
+        
+    if id==3:
+        NBLAYERS_UNET = 3
+        
+    if id==4:
+        NBLAYERS_UNET = 5
+        
+    if id==5:
+        DROPOUT_RATE = 0.0
+            
+    if id==6:
+        DATA_AUGMENTATION = False    
+ 
+    if id==7:
+        LR = 0.1
+        
+    if id==8:
+        RMS_OPT = True 
+        
+
+    model_path = './trained_model/'
+    model_name = 'Unet_' + ACTIVATION + '_f' + str(NBFILTERS_L1_UNET) + '_b' + str(BATCH_SIZE) + '_l'+ str(NBLAYERS_UNET) + '_do' + str(DROPOUT_RATE) +'_Std'
+    if BATCHNORM_ON == True:
+        model_name = model_name + '_BN'
+    else:
+        model_name = model_name + '_noBN'
+            
+    if DATA_AUGMENTATION == True:
+        model_name = model_name + '_DA'
+    else:
+        model_name = model_name + '_noDA'
+        
+    if RMS_OPT == True:
+        model_name = model_name + '_RMS'
+        
+    if LR == 0.1:
+        model_name = model_name + '_lr-1'
+        
+    model_filename = model_path + model_name + '_input'+ str(img_size) +'.h5'
+    print(" -> model_filename : ", model_filename)
+
+    if 'model' in locals(): 
+        del model
+
+    # tf.keras.backend.clear_session()
+
+    # Load training curves
+    im_path = model_path + model_name + '_input'+ str(img_size) + '_loss.png'
+    im = mpimg.imread(im_path)
+    
+    # Display curves
+    f, ax = plt.subplots(1, 1, figsize=(12,12))
+    ax.imshow(im)
+    ax.axis('off')
+        
+    # Load training curves
+    im_path = model_path + model_name + '_input'+ str(img_size) + '_metric.png'
+    im = mpimg.imread(im_path)
+    
+    # Display curves
+    f, ax = plt.subplots(1, 1, figsize=(12,12))
+    ax.imshow(im)
+    ax.axis('off')        
+        
+    # Load the network with its custom functions
+    model = models.load_model(model_filename, custom_objects={'dice_coef': dice_coef, 'adaptive_loss': adaptive_loss, 'dice_loss': dice_loss})    
+        
+    return model
 
 
 # Load dataset
-def load_ACDC_dataset(images_path, IMG_SIZE):
+def load_CAMUS_dataset(images_path, img_size):
     
     # Create list of files to load for both images and the corresponding labels
     masks_files  = glob.glob("{}/labels/*.png".format(images_path)) 
@@ -21,8 +114,8 @@ def load_ACDC_dataset(images_path, IMG_SIZE):
     masks_files_rnd = [masks_files[i] for i in permutation_index]   
     
     # Reading images and corresponding labels
-    X = ReadImages(images_files_rnd, size=(IMG_SIZE, IMG_SIZE))
-    y = ReadMasks(masks_files_rnd, size=(IMG_SIZE, IMG_SIZE))    
+    X = ReadImages(images_files_rnd, size=(img_size, img_size))
+    y = ReadMasks(masks_files_rnd, size=(img_size, img_size))    
     
     # Return the images (X) and corresponding labels (y) into numpy array 
     return X, y
